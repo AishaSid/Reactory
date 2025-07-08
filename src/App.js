5828c6f5944e1a2d34bc3a5e10f3f6e46c016dc0
AishaSid <114816803+AishaSid@users.noreply.github.com>
2025-07-08 12:17:46 +0500

    feat: Class Props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// Components
 import FC from './Components/FunctionalComp';
 import CC from './Components/ClassComp';
 import { ClassComp1 } from './Components/ClassComp'; 
@@ -6,20 +8,32 @@ import Click from './Components/Click';
 import Counter from './Components/Counter';
 import ParentComp from './Components/ParentComp';
 
+//Props
+import CProps from './ClassProps/cProps';
+
+
 function App() {
   return <div>
+     {/* App Components */}
     <h1>Welcome to the React Components</h1>
   <p>Components return HTML</p>
   <FC></FC>
   <CC></CC>
   <ClassComp1></ClassComp1>
-    <p>App imports Click and Counter</p>
   <Click></Click>
   <Counter></Counter>
-
+  <p>App imports Click and Counter </p>
   <ParentComp></ParentComp>
-  </div>
-  ;
+  <hr />
+  {/* Props */}
+<h1>Class Props</h1>
+<CProps name="Aisha Siddiqa"></CProps>
+<CProps name="Asim Khan"></CProps>
+  <hr />
+   {/* States */}
+  
+  
+  </div>;
 }
 
 export default App;
---
+++ src/ClassProps/cProps.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class cProps extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello I am {this.props.name} </p>
+      </div>
+    )
+  }
+};
+
+export default cProps;
